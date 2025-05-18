d1b7c6bba4d7e80e9a984865d4b6b6f6993b2f0a
Roman Chistokhodov <freeslave93@gmail.com>
2025-05-19 01:00:35 +0300

    Add tests for InventoryHudSpec

--- tests/inventory_test.cpp
+++ tests/inventory_test.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 #include "inventory.h"
+#include "hud_inventory.h"
+#include "color_utils.h"
 
 const char inventorySpecs[] = R"(
 {
@@ -60,4 +62,60 @@ TEST(InventorySpec, Parse)
 
 		EXPECT_EQ(spec->pickupEntTemplateName, "red_battery");
 	}
+}
+
+const char inventoryHudSpecs[] = R"(
+{
+	"default_sprite_alpha": 225,
+	"text_alpha": 200,
+	"items": {
+		"battery_blue": {
+			"sprite": "item_battery",
+			"position": "bottom",
+			"color": [0, 100, 255]
+		},
+		"keycard": {
+			"sprite": "item_keycard",
+			"position": "topleft",
+			"color": "204 204 255"
+		},
+		"armorvest": {
+			"sprite": "item_armorvest",
+			"position": "topright"
+		}
+	}
+}
+)";
+
+TEST(InventoryHudSpec, Parse)
+{
+	InventoryHudSpec ihs;
+
+	ASSERT_TRUE(ihs.ReadFromContents(inventoryHudSpecs, ""));
+
+	EXPECT_EQ(ihs.DefaultSpriteAlpha(), 225);
+	EXPECT_EQ(ihs.TextAlpha(), 200);
+
+	const InventoryItemHudSpec* batterySpec = ihs.GetInventoryItemSpec("battery_blue");
+	ASSERT_TRUE(batterySpec != nullptr);
+
+	EXPECT_STREQ(batterySpec->spriteName,  "item_battery");
+	EXPECT_TRUE(batterySpec->colorDefined);
+	EXPECT_EQ(batterySpec->packedColor, PackRGB(0, 100, 255));
+	EXPECT_EQ(batterySpec->position, INVENTORY_PLACE_BOTTOM_CENTER);
+
+	const InventoryItemHudSpec* keycardSpec = ihs.GetInventoryItemSpec("keycard");
+	ASSERT_TRUE(keycardSpec != nullptr);
+
+	EXPECT_STREQ(keycardSpec->spriteName,  "item_keycard");
+	EXPECT_TRUE(keycardSpec->colorDefined);
+	EXPECT_EQ(keycardSpec->packedColor, PackRGB(204, 204, 255));
+	EXPECT_EQ(keycardSpec->position, INVENTORY_PLACE_TOP_LEFT);
+
+	const InventoryItemHudSpec* armorVestSpec = ihs.GetInventoryItemSpec("armorvest");
+	ASSERT_TRUE(armorVestSpec != nullptr);
+
+	EXPECT_STREQ(armorVestSpec->spriteName,  "item_armorvest");
+	EXPECT_FALSE(armorVestSpec->colorDefined);
+	EXPECT_EQ(armorVestSpec->position, INVENTORY_PLACE_TOP_RIGHT);
 }
