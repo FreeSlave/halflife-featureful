84578f8056008844a4011ae18b5139af8c897f53
Roman Chistokhodov <freeslave93@gmail.com>
2026-01-20 03:20:10 +0300

    Make scientists smell garbage scent

--- dlls/scientist.cpp
+++ dlls/scientist.cpp
@@ -883,6 +883,7 @@ int CScientist::DefaultISoundMask()
 			bits_SOUND_COMBAT |
 			bits_SOUND_CARCASS |
 			bits_SOUND_MEAT |
+			bits_SOUND_GARBAGE |
 			bits_SOUND_DANGER |
 			bits_SOUND_PLAYER;
 }
