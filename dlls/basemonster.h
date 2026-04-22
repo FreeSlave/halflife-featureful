fc73f41eaab7afd18e4a3ae17f6beeccb0932f25
Roman Chistokhodov <freeslave93@gmail.com>
2026-04-22 03:25:21 +0300

    Remove MakeDamageBloodDecal (unused function)

--- dlls/basemonster.h
+++ dlls/basemonster.h
@@ -372,7 +372,6 @@ public:
 	virtual void PrescheduleThink() { return; }
 
 	bool GetEnemy( bool forcePopping );
-	void MakeDamageBloodDecal( int cCount, float flNoise, TraceResult *ptr, const Vector &vecDir );
 	virtual float HeadHitGroupDamageMultiplier();
 	void ApplyHitGroupDamageMultiplier(DamageInfo& damageInfo, int hitgroup);
 	void TraceAttack( entvars_t *pevInflictor, entvars_t *pevAttacker, const DamageInfo& damageInfo, Vector vecDir, TraceResult *ptr ) override;
